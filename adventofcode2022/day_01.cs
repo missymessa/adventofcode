1b659cfc52f10e8218e5d904f792ca714e259105
Missy Messa <mjanecke@microsoft.com>
2022-11-30 21:14:30 -0800

    Day 1, 2022

---
+++ adventofcode2022/day_01.cs
@@ -0,0 +1,42 @@
+﻿using AdventOfCSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode2022
+{
+    public static class DayOne
+    {
+        public static void Execute()
+        {
+            List<string> numbersInput = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "input", "day_01.txt")).ToList();
+
+            int currentTotal = 0;
+
+            List<int> totals = new List<int>();
+
+            foreach(var input in numbersInput)
+            {                
+                if(!input.IsNullOrEmpty())
+                {
+                    currentTotal += Convert.ToInt32(input);
+                }
+                else
+                {
+                    totals.Add(currentTotal);
+                    currentTotal = 0;
+                }
+            }
+
+            totals.Sort();
+            int highestTotal = totals.Last();
+
+            Console.WriteLine("Problem 1: {0}", highestTotal);
+
+            Console.WriteLine("Problem 2: {0}", totals.TakeLast(3).Sum(x => x));
+
+        }
+    }
+}
