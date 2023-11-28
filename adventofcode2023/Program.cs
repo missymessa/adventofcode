3a93fbe9aa86577a5751d9fb3eafffb7af2566b9
missymessa <mjanecke@microsoft.com>
2023-11-28 11:46:41 -0800

    Prep for 2023

---
+++ adventofcode2023/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace adventofcode2023
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code - Day One");
+            new DayOne().Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Two");
+            //new DayTwo().Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Three");
+            //DayThree.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Four");
+            //DayFour.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Five");
+            //DayFive.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Six");
+            //DaySix.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Seven");
+            //DaySeven.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Eight");
+            //DayEight.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Nine");
+            //DayNine.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Ten");
+            //DayTen.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Eleven");
+            //DayEleven.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Twelve");
+            //DayTwelve.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Thirteen");
+            //new DayThirteen().Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Fourteen");
+            //DayFourteen.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Fifteen");
+            //new DayFifteen().Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Sixteen");
+            //new DaySixteen().Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Seventeen");
+            //DaySeventeen.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Eighteen");
+            //DayEighteen.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Nineteen");
+            //DayNineteen.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Twenty");
+            //DayTwenty.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Twenty-One");
+            //DayTwentyOne.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Twenty-Two");
+            //DayTwentyTwo.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Twenty-Three");
+            //DayTwentyThree.Execute();
+
+            //Console.WriteLine("\nAdvent of Code - Day Twenty-Four");
+            //DayTwentyFour.Execute();
+        }
+    }
+}
---
+++ adventofcode2023/day_01.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode2023
+{
+    public class DayOne : DayBase<int>
+    {
+        public DayOne() : base("day_01.txt") { }
+
+        public DayOne(string fileName) : base(fileName) { }
+
+        public override int Problem1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Problem2()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
