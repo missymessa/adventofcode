362298e6ed20a3b6c48f55f96b65521f0379df06
missymessa <mjanecke@microsoft.com>
2023-12-11 08:52:37 -0800

    Day 10 2023 - incomplete part 2

---
+++ adventofcode2023.tests/day_10.Tests.cs
@@ -0,0 +1,43 @@
+using adventofcode2023;
+
+namespace adventofcode2023.tests
+{
+    public class DayTenTests
+    {
+        [Test]
+        public void Problem1Example()
+        {
+            var day = new day_10("day_10_ex.txt");
+
+            Assert.That(day.Problem1(), Is.EqualTo(8));
+        }
+
+        [Test]
+        [TestCase("day_10_ex2.txt", 4)]
+        [TestCase("day_10_ex3.txt", 4)]
+        [TestCase("day_10_ex4.txt", 8)]
+        [TestCase("day_10_ex5.txt", 10)]
+        public void Problem2Example(string filename, long result)
+        {
+            var day = new day_10(filename);
+
+            Assert.That(day.Problem2(), Is.EqualTo(result));
+        }
+
+        [Test]
+        public void Problem1Actual()
+        {
+            var day = new day_10();
+
+            Assert.That(day.Problem1(), Is.EqualTo(7107));
+        }
+
+        [Test]
+        public void Problem2Actual()
+        {
+            var day = new day_10();
+
+            Assert.That(day.Problem2(), Is.EqualTo(1068));
+        }
+    }
+}
---
+++ adventofcode2023/day_10.cs
@@ -0,0 +1,234 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using adventofcode.util;
+
+namespace adventofcode2023
+{
+    public class day_10 : DayBase<long>
+    {
+        private Dictionary<(int x, int y), List<(int x, int y)>> adjacencyList = new Dictionary<(int x, int y), List<(int x, int y)>>();
+        private long maxDistance = 0;
+        private int maxX = 0;
+        private int maxY = 0;
+        private (int x, int y) startingPosition = (0, 0);
+        private string[] updatedInput;
+
+
+        public day_10() : base("day_10.txt")
+        {
+            Console.WriteLine("Advent of Code - Day Ten");
+            SetUp();
+        }
+
+        public day_10(string fileName) : base(fileName) 
+        {
+            SetUp();
+        }
+
+        public override long Problem1()
+        {
+            return FindFarthestDistance(startingPosition, maxX, maxY);
+        }
+
+        public override long Problem2()
+        {
+            string[] display = new string[maxY];
+
+            // count internal spaces            
+            long enclosedSpaces = 0;
+
+            int y = 0;
+
+            foreach(var input in updatedInput)
+            {
+                char[] inputChar = input.ToCharArray();
+                bool isOpen = true;
+
+                for (int x = 0; x < inputChar.Length; x++)
+                {                    
+                    char c = inputChar[x];
+                    List<(int x, int y)> value;
+                    bool isInLoop = adjacencyList.TryGetValue((x, y), out value);
+
+                    if (isInLoop && value.Count == 2 && 
+                        (c == '|' || 
+                        (c == 'F' && LookAhead(inputChar, x, 'J', '-')) || 
+                        (c == 'L' && LookAhead(inputChar, x, '7', '-'))))
+                        isOpen = !isOpen;
+
+                    if ((c == '.' || (c != '.' && (!isInLoop || value.Count < 2))) && !isOpen)
+                    {
+                        enclosedSpaces++;
+                        inputChar[x] = 'I';
+                    }
+                }
+
+                display[y] = new string(inputChar);
+
+                y++;
+            }
+
+            foreach(var line in display)
+            {
+                Console.WriteLine(line);
+            }
+
+            return enclosedSpaces;
+        }
+
+        private bool LookAhead(char[] line, int startingIndex, char ending, char ignore)
+        {
+            int index = startingIndex + 1;
+
+            while(index < line.Length)
+            {
+                if(line[index] == ending)
+                    return true;
+
+                if (line[index] != ignore)
+                    return false;
+
+                index++;
+            }
+
+            return false;
+        }
+
+        private void SetUp()
+        {
+            maxX = _input[0].Length;
+            maxY = _input.Count;
+            updatedInput = new string[maxY];
+
+            int y = 0;
+
+            foreach (var input in _input)
+            {
+                char[] inputChar = input.ToCharArray();
+
+                for (int x = 0; x < inputChar.Length; x++)
+                {
+                    if (inputChar[x] != '.')
+                    {
+                        adjacencyList[(x, y)] = new List<(int x, int y)>();
+                    }
+
+                    switch (inputChar[x])
+                    {
+                        case '|':
+                            if (y > 0 && (_input[y - 1][x] != 'L' && _input[y - 1][x] != 'J' && _input[y - 1][x] != '-')) AddEdge((x, y), (x, y - 1));
+                            if (y < maxY - 1 && (_input[y + 1][x] != 'F' && _input[y + 1][x] != '7' && _input[y + 1][x] != '-')) AddEdge((x, y), (x, y + 1));
+                            break;
+                        case '-':
+                            if (x > 0 && (_input[y][x - 1] != '7' && _input[y][x - 1] != 'J' && _input[y][x - 1] != '|')) AddEdge((x, y), (x - 1, y));
+                            if (x < maxX - 1 && (_input[y][x + 1] != 'L' && _input[y][x + 1] != 'F' && _input[y][x + 1] != '|')) AddEdge((x, y), (x + 1, y));
+                            break;
+                        case 'L':
+                            if (y > 0 && (_input[y - 1][x] != 'L' && _input[y - 1][x] != 'J' && _input[y - 1][x] != '-')) AddEdge((x, y), (x, y - 1));
+                            if (x < maxX - 1 && (_input[y][x + 1] != 'L' && _input[y][x + 1] != 'F' && _input[y][x + 1] != '|')) AddEdge((x, y), (x + 1, y));
+                            break;
+                        case 'J':
+                            if (y > 0 && (_input[y - 1][x] != 'L' && _input[y - 1][x] != 'J' && _input[y - 1][x] != '-')) AddEdge((x, y), (x, y - 1));
+                            if (x > 0 && (_input[y][x - 1] != '7' && _input[y][x - 1] != 'J' && _input[y][x - 1] != '|')) AddEdge((x, y), (x - 1, y));
+                            break;
+                        case '7':
+                            if (y < maxY - 1 && (_input[y + 1][x] != 'F' && _input[y + 1][x] != '7' && _input[y + 1][x] != '-')) AddEdge((x, y), (x, y + 1));
+                            if (x > 0 && (_input[y][x - 1] != '7' && _input[y][x - 1] != 'J' && _input[y][x - 1] != '|')) AddEdge((x, y), (x - 1, y));
+                            break;
+                        case 'F':
+                            if (y < maxY - 1 && (_input[y + 1][x] != 'F' && _input[y + 1][x] != '7' && _input[y + 1][x] != '-')) AddEdge((x, y), (x, y + 1));
+                            if (x < maxX - 1 && (_input[y][x + 1] != 'L' && _input[y][x + 1] != 'F' && _input[y][x + 1] != '|')) AddEdge((x, y), (x + 1, y));
+                            break;
+                        case 'S':
+                            startingPosition = (x, y);
+                            List<char> possiblePipes = new List<char>();
+                            // based on what tiles are around it, determine the shape of the pipe at S
+                            if (y > 0 && (_input[y - 1][x] == '|' || _input[y - 1][x] == '7' || _input[y - 1][x] == 'F'))
+                            {
+                                AddEdge((x, y), (x, y - 1));
+                                possiblePipes.AddRange(new[] { '|', 'J', 'L' });
+                            }
+
+                            if (y < maxY - 1 && (_input[y + 1][x] == '|' || _input[y + 1][x] == 'J' || _input[y + 1][x] == 'L'))
+                            {
+                                AddEdge((x, y), (x, y + 1));
+                                possiblePipes.AddRange(new[] { '|', 'F', '7' });
+                            }
+
+                            if (x > 0 && (_input[y][x - 1] == '-' || _input[y][x - 1] == 'F' || _input[y][x - 1] == 'L'))
+                            {
+                                AddEdge((x, y), (x - 1, y));
+                                possiblePipes.AddRange(new[] { '-', '7', 'J' });
+                            }
+
+                            if (x < maxX - 1 && (_input[y][x + 1] == '-' || _input[y][x + 1] == '7' || _input[y][x + 1] == 'J'))
+                            {
+                                AddEdge((x, y), (x + 1, y));
+                                possiblePipes.AddRange(new[] { '-', 'F', 'L' });
+                            }
+
+                            var newS = possiblePipes.GroupBy(c => c)
+                                     .Where(group => group.Count() > 1)
+                                     .Select(group => group.Key)
+                                     .FirstOrDefault();
+
+                            inputChar[x] = newS;
+
+                            break;
+                    }
+                }
+
+                updatedInput[y] = new string(inputChar);
+
+                y++;
+            }
+        }
+
+        public void AddEdge((int x, int y) v, (int x, int y) w)
+        {
+            adjacencyList[v].Add(w);
+        }
+
+        public long FindFarthestDistance((int x, int y) startVertex, int maxX, int maxY)
+        {
+            bool[,] visited = new bool[maxX, maxY]; // Assuming maximum x and y values
+
+            // Initialize a queue for BFS
+            Queue<(int x, int y)> queue = new Queue<(int x, int y)>();
+            queue.Enqueue(startVertex);
+            visited[startVertex.Item1, startVertex.Item2] = true;
+
+            // Initialize distance to 0
+            long distance = 0;
+
+            while (queue.Count > 0)
+            {
+                int levelSize = queue.Count;
+
+                // Process all vertices at the current level
+                for (int i = 0; i < levelSize; ++i)
+                {
+                    (int x, int y) vertex = queue.Dequeue();
+
+                    foreach (var neighbor in adjacencyList[vertex])
+                    {
+                        if (!visited[neighbor.Item1, neighbor.Item2])
+                        {
+                            visited[neighbor.Item1, neighbor.Item2] = true;
+                            queue.Enqueue(neighbor);
+                        }
+                    }
+                }
+
+                // Increment distance after processing each level
+                distance++;
+            }
+
+            // Subtract 1 to get the farthest distance from the starting vertex
+            return distance - 1;
+        }
+    }
+}
