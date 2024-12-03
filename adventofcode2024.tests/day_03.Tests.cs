dcece302ff80f1658d9c8b3fb507bbae61672436
missymessa <mjanecke@microsoft.com>
2024-12-03 13:19:10 -0800

    Day 3

---
+++ adventofcode2024.tests/day_03.Tests.cs
@@ -0,0 +1,39 @@
+﻿using adventofcode2024;
+
+namespace adventofcode2024.tests
+{
+    public class DayThreeTests
+    {
+        [Test]
+        public void Problem1Example()
+        {
+            var day = new day_3("day_03_ex.txt");
+
+            Assert.That(day.Problem1(), Is.EqualTo(161));
+        }
+
+        [Test]
+        public void Problem2Example()
+        {
+            var day = new day_3("day_03_ex2.txt");
+
+            Assert.That(day.Problem2(), Is.EqualTo(48));
+        }
+
+        [Test]
+        public void Problem1Actual()
+        {
+            var day = new day_3();
+
+            Assert.That(day.Problem1(), Is.EqualTo(171183089));
+        }
+
+        [Test]
+        public void Problem2Actual()
+        {
+            var day = new day_3();
+
+            Assert.That(day.Problem2(), Is.EqualTo(63866497));
+        }
+    }
+}
---
+++ adventofcode2024/day_03.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using adventofcode.util;
+
+namespace adventofcode2024
+{
+    public class day_3 : DayBase<int>
+    {
+        public day_3() : base("day_03.txt")
+        {
+            Console.WriteLine("Advent of Code - Day Three");
+        }
+
+        public day_3(string fileName) : base(fileName) { }
+
+        public override int Problem1()
+        {
+            return ExtractResult(_input[0]);
+        }
+
+        private int ExtractResult(string line)
+        {
+            int result = 0;
+
+            var matches = Regex.Matches(line, @"mul\(\d{1,3},\d{1,3}\)");
+
+            foreach (var match in matches)
+            {
+                int num1, num2;
+
+                RegexHelper.Match(match.ToString(), @"mul\((\d{1,3}),(\d{1,3})\)", out num1, out num2);
+
+                result += num1 * num2;
+            }
+
+            return result;
+        }
+
+        public override int Problem2()
+        {
+            int result = 0;
+
+            var lineSplit = _input[0].Split("don't()");
+
+            result = ExtractResult(lineSplit[0]);
+
+            for (int i = 1; i < lineSplit.Length; i++)
+            {
+                var lineSplitDo = lineSplit[i].Split("do()");
+                if (lineSplitDo.Length > 1)
+                {
+                    for (int j = 1; j < lineSplitDo.Length; j++)
+                    {
+                        result += ExtractResult(lineSplitDo[j]);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
